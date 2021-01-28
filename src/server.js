3e63a797f8a55bb430a149412d1a89742ed0b6a5
Juan David Ramirez Mendoza <c4pjackie@gmail.com>
2021-01-27 22:40:35 -0500

    Project Structured changed, nodemon configured for hot reload, experimenting with middlewares and example controllers

---
+++ src/server.js
@@ -0,0 +1,33 @@
+import express, { response } from "express";
+import { json, urlencoded } from "body-parser";
+import morgan from "morgan";
+import cors from "cors";
+
+export const app = express();
+
+app.disable("x-powered-by");
+
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+const log = (req, res, next) => {
+  console.log("Logging");
+
+  next();
+};
+app.get("/", (req, res) => {
+  res.send({ message: "hello" });
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send({ message: "ok" });
+});
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log(`server is on 3000`);
+  });
+};
